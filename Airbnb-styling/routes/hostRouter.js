3f8f238ae843560115fd761f9f34dcb1627fc989
ghazanfar <ghznfr0078@gmail.com>
2024-12-21 09:35:37 +0500

    tailwind added

---
+++ Airbnb-styling/routes/hostRouter.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const hostRouter = express.Router()
+const rootDir = require('../utils/pathUtils')
+
+hostRouter.use(express.static(path.join(rootDir, 'public')))
+hostRouter.get('/add-home', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-home.html'))
+})
+
+hostRouter.post('/add-home', (req, res, next) => {
+  console.log(req.body);
+  res.sendFile(path.join(rootDir, 'views', 'home-added.html'))
+})
+
+module.exports = hostRouter
